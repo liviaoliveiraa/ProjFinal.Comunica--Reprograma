784d929076bda00d4741d9391fc9998dcc98f8cf
Lívia Oliveira <livia.oliveira.almeida@hotmail.com>
2021-09-06 23:30:38 -0300

    alterando arq usuariasrouter

--- src/routers/usuariasRouter.js
+++ src/routers/usuariasRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/usuariasController')
 
-router.post('/create', controller.create)
+router.post('/createusuaria', controller.create)
 router.post('/login', controller.login)
 
 module.exports = router
