d8a00b6c8b7fea7180c48568d8b8f3d1d626f0b7
Lívia Oliveira <livia.oliveira.almeida@hotmail.com>
2021-09-09 20:55:31 -0300

    alterando rota infomateria-delete

--- src/routers/infoMaterias.router.js
+++ src/routers/infoMaterias.router.js
@@ -5,6 +5,6 @@ const controller = require("../controllers/infoMateriasController")
 router.get('/', controller.rules)
 router.get('/todas', controller.getAll)
 router.post('/create', controller.createInfo)
-router.delete('/:id', controller.deleteById)
+router.delete('/id/:id', controller.deleteById)
 
 module.exports = router;
